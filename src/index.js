df4a720bdc58e0bd2493b19b29111828eec0949e
Edgar <amirbekianedgar@gmail.com>
2019-06-23 16:58:14 +0400

    Initialize application with create react app, remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RenderApp = Component => ReactDOM.render(
+    <Component />,
+    document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+RenderApp(App);
+
+if (process.env.NODE_ENV === 'development') {
+    if (module.hot) {
+        module.hot.accept('./App', () => {
+            // console.log('Hot reload just happened');
+            const NextApp = require('./App').default;
+            RenderApp(NextApp);
+        });
+    }
+}
