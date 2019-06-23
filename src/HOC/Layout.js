f409c7fec5b65f8b0dd57dbfe3cd3789d32e587a
Edgar <amirbekianedgar@gmail.com>
2019-06-23 17:34:28 +0400

    add static files

---
+++ src/HOC/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//styles
+
+//app modules
+
+//3rd party libs
+
+const Layout = () => {
+    return (
+        <div>
+            abbe
+        </div>
+    );
+};
+
+export default Layout;
