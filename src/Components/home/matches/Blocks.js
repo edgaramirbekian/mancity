70f690329ea943ef607aa887f2454c663da25241
Edgar <amirbekianedgar@gmail.com>
2019-07-04 23:08:01 +0400

    add matches blocka

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { firebaseMatches } from "../../../firebase";
+
+//app modules
+import { firebaseLooper, reverseArray } from '../../utils/miscs';
+import MatchesBlock from '../../utils/matches_block';
 
 const Blocks = () => {
+
+    const [matches, setMatches] = useState([]);
+
+    useEffect(() => {
+        firebaseMatches.limitToLast(6).once("value").then((snapshot) => {
+            const matches = firebaseLooper(snapshot);
+            setMatches(() => {return reverseArray(matches)});
+        })
+    }, []);
+
+    const showMatches = () => {
+        if (matches) {
+            matches.map((match,idx) => (
+                <div key={idx} className="item">
+                    <div className="wrapper">
+                        <MatchesBlock match={match} />
+                    </div>
+                </div>
+            ))
+        } else {
+
+        }
+    };
+
     return (
-        <div>
-            
+        <div className="home_matches">
+            {showMatches()}
         </div>
     );
 };
